bd55686ce2abc2a786c559717ee6ccd0f916ccdb
洪浩岚 <1457315179@qq.com>
2024-05-08 11:13:33 +0800

    draft no.1

--- index.js
+++ index.js
@@ -3,10 +3,20 @@ const app = express();
 const bodyParser = require('body-parser');
 const fs = require('fs');
 
+// specifying the static files directory
 app.use(express.static('public'));
 
+// handling get request for browser
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/introductionPage.html');
+});
+
+app.get('/aboutPage', (req, res) => {
+    res.sendFile(__dirname + '/public/aboutPage.html');
+});
+
+app.get('/quiz', (req, res) => {
+    res.sendFile(__dirname + '/public/quizApplicationPage.html');
 });
 
 app.listen(8080, () => {
